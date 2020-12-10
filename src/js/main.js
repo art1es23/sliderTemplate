3166d1860a4209ba2e92b4694de48cb7339cbba3
Denys Symak <dreamwinamounts@gmail.com>
2020-12-10 12:28:54 +0200

    v.1.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,236 +1,229 @@
-import * as PIXI from 'pixi.js';
-/*
-import 'pixi-filters';
-*/
-import {TimelineMax} from 'gsap';
 
-let app = new PIXI.Application({
-        width: window.innerWidth,         // default: 800
-        height: window.innerHeight,        // default: 600
-        antialias: true,    // default: false
-        transparent: false, // default: false
-        resolution: 1,       // default: 1
-        backgroundColor: 0xFFBB55
+const prev = document.querySelector('.btn--prev');
+const next = document.querySelector('.btn--next');
+const slider = document.querySelector('.slider--wrapper');
+const slides = document.querySelectorAll('.slider__item');
+const dots = document.querySelectorAll('.slider-dots__item');
+
+let index = 0;
+
+const active = (array, n) => {
+    array.forEach(item => {
+        item.classList.remove('active');
+    })
+    array[n].classList.add('active');
+}
+
+const activeSlide = () => active(slides, index)
+const activeDots = () => active(dots, index)
+const currentActive = (x) => {
+    activeSlide(x);
+    activeDots(x);
+}
+
+const nextSlide = () => {
+    if (index === slides.length - 1) {
+        index = 0;
+        currentActive(index)
     }
-);
-
-document.body.appendChild(app.view);
-
-const loader = PIXI.Loader.shared;
-
-loader
-    .add("img/6.jpg")
-    .load(setup);
-
-function setup() {
-
-        //INITIALIZATION
-        let container = new PIXI.Container();
-        let bgContainer = new PIXI.Container();
-        let textContainer = new PIXI.Container();
-
-        app.stage.addChild(container);
-        container.addChild(bgContainer);
-        container.addChild(textContainer);
-
-        //BACKGROUND STAGE
-        let bg = PIXI.Sprite.from("img/6.jpg");
-
-        bg.width = window.innerWidth;
-        bg.height = window.innerHeight;
-
-        bgContainer.addChild(bg);
-
-        //SHOCKWAVE
-/*
-        bgContainer.filters = [new PIXI.filters.ShockwaveFilter()];
-*/
-
-
-        //TEXT STAGE
-        let basicText = new PIXI.Text('Basic text in pixi', {
-                fontFamily: 'Arial Narrow',
-                fontSize: 136,
-                fontStyle: 'italic',
-                fontWeight: 'bold',
-                fill: '#ffffff',
-                wordWrap: true,
-                wordWrapWidth: 440
-        })
-
-        basicText.x = -300;
-        basicText.y = 100;
-        basicText.alpha = 0;
-
-        textContainer.addChild(basicText);
-
-        //DISPLACEMENT STAGE
-        let displacementSprite = PIXI.Sprite.from("img/displacement.png");
-        let displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
-
-        app.stage.addChild(displacementSprite);
-
-        displacementFilter.scale.x = 200;
-        displacementFilter.scale.y = 300;
-
-        textContainer.filters = [displacementFilter];
-
-        let tl = new TimelineMax();
-        tl
-/*            .to(shockWave, 3, {time: 1, amplitude: 10})*/
-            .to( displacementFilter.scale, 1, {x: 0.1, y: 0.1})
-            .to(basicText.position, 1, {x: 100}, 0)
-            .to(basicText, 1, {alpha: 1}, 0);
-
-        document.body.addEventListener('click', () => {
-                let tl = new TimelineMax();
-                tl
-                    .to( displacementFilter.scale, 1, {x: 300, y: 200})
-                    .to(basicText.position, 1, {x: 400}, 0)
-                    .to(basicText, 1, {alpha: 0}, 0);
-        });
-}
-
-app.ticker.add(function (delta) {
-});
-
-
-
-/*
-let app = new PIXI.Application({
-        width: window.innerWidth,         // default: 800
-        height: window.innerHeight,        // default: 600
-        antialias: true,    // default: false
-        transparent: false, // default: false
-        resolution: 1,       // default: 1
-        backgroundColor: 0xFFBB55
+    else {
+        index++;
+        currentActive(index)
     }
-);
-
-document.body.appendChild(app.view);
-
-const loader = PIXI.Loader.shared;
-
-loader
-    .add("img/atlas.json")
-    .load(setup);
-
-function setup () {
-        let id = loader.resources["img/atlas.json"].textures;
-        let mino = new PIXI.Sprite(id["mino.png"]);
-
-        mino.anchor.set(0.5, 0.5);
-        mino.scale.set(0.5);
-
-        mino.x = window.innerWidth / 2;
-        mino.y = window.innerHeight / 2;
-        app.stage.addChild(mino);
-
-        let box = new PIXI.Graphics();
-        box.beginFill(0xccff99);
-        box.drawRect(0,0,75, 75);
-        box.endFill();
-        box.x = 250;
-        box.y = 250;
-        app.stage.addChild(box);
-
-        app.ticker.add((delta) => {
-                if (hitTestRectangle(mario, mino)) {
-                        /!*box.tint = 0xff3300;*!/
-                        mino.scale.set(0.6);
-                } else {
-                        mino.scale.set(0.5);
-                }
-        });
-
-
-        let mario = new PIXI.Sprite(id["mario.png"]);
-
-        mario.anchor.set(0.5, 0.5);
-        mario.scale.set(0.25);
-
-        mario.x = 300;
-        mario.y = window.innerHeight / 2;
-        app.stage.addChild(mario);
-
-        app.ticker.add((delta) => {
-                mario.rotation -= delta * 0.1;
-        });
-        app.stage.interactive = true;
-        app.stage.on("pointermove", moveMario);
-
-        function moveMario(e) {
-                let pos = e.data.global;
-
-                mario.x = pos.x;
-                mario.y = pos.y;
-        }
-
-        document.addEventListener("keydown", function () {
-                if (event.keyCode == 37) {
-                        mino.x -= 15;
-                }
-                if (event.keyCode == 38) {
-                        mino.y -= 15;
-                }
-                if (event.keyCode == 39) {
-                        mino.x += 15;
-                }
-                if (event.keyCode == 40) {
-                        mino.y += 15;
-                }
-        });
 
 }
 
-function hitTestRectangle(r1, r2) {
+const prevSlide = () => {
+    if (index === 0) {
+        index = slides.length - 1;
+        currentActive(index)
+    }
+    else {
+        index--;
+        currentActive(index)
+    }
+}
 
-        //Define the variables we'll need to calculate
-        let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+prev.addEventListener('click', prevSlide);
+next.addEventListener('click', nextSlide);
+/*
 
-        //hit will determine whether there's a collision
-        hit = false;
+function slideNext () {
+    const slidesVisible = document.querySelectorAll('.slider__item');
+    let offset2 = 0;
 
-        //Find the center points of each sprite
-        r1.centerX = r1.x + r1.width / 2;
-        r1.centerY = r1.y + r1.height / 2;
-        r2.centerX = r2.x + r2.width / 2;
-        r2.centerY = r2.y + r2.height / 2;
-
-        //Find the half-widths and half-heights of each sprite
-        r1.halfWidth = r1.width / 2;
-        r1.halfHeight = r1.height / 2;
-        r2.halfWidth = r2.width / 2;
-        r2.halfHeight = r2.height / 2;
-
-        //Calculate the distance vector between the sprites
-        vx = r1.centerX - r2.centerX;
-        vy = r1.centerY - r2.centerY;
-
-        //Figure out the combined half-widths and half-heights
-        combinedHalfWidths = r1.halfWidth + r2.halfWidth;
-        combinedHalfHeights = r1.halfHeight + r2.halfHeight;
-
-        //Check for a collision on the x axis
-        if (Math.abs(vx) < combinedHalfWidths) {
-
-                //A collision might be occurring. Check for a collision on the y axis
-                if (Math.abs(vy) < combinedHalfHeights) {
-
-                        //There's definitely a collision happening
-                        hit = true;
-                } else {
-
-                        //There's no collision on the y axis
-                        hit = false;
-                }
-        } else {
-
-                //There's no collision on the x axis
-                hit = false;
-        }
-
-        //`hit` will be either `true` or `false`
-        return hit;
+    for (let i = 0; i < slidesVisible.length; i++) {
+        slidesVisible[i].style.left = offset2 * 512 + 512 - 'px';
+        offset2++;
+    }
 }
 */
+
+/*
+rightBtn.addEventListener('click', slideNext);
+leftBtn.addEventListener('click', slidePrev);
+
+function slideNext(e) {
+    e.preventDefault();
+}
+*/
+
+
+/*
+const ages = [1,2,3,0,4,5,6];
+
+let ar = 0;
+for (let i = 0; i < ages.length; i++) {
+    if (ages[i] === 0) break;
+    ar = ar + ages[i];
+    console.log(ages[i]);
+}
+console.log(ar)
+*/
+
+
+
+/*let regExp = /^[a-zA-Z]{1,10} [a-zA-Z]{1,20}$/;
+
+console.log(regExp.test('Denys Symak'))*/
+/*
+const f1 = document.forms['f1'];
+*/
+
+/*
+const btnAddUser = document.querySelector('.add');
+const btnEditUser = document.querySelector('.editUser');
+let tableUsers = document.querySelector('.menu');
+let tableUsersWrapper = document.querySelector('.menu tbody');
+
+let count = 0;
+document.getElementById('tableUsers').addEventListener('click', tableClick,false);
+
+btnAddUser.addEventListener('click', () => {
+    let loginValue = f1.login.value;
+    let passwordValue = f1.password.value;
+    let mailValue = f1.mail.value;
+
+    count++;
+
+    let tableRow = document.createElement('tr');
+    tableRow.className = 'menu--wrapper';
+    tableRow.innerHTML = `
+            <td class="menu__item menu__item--number">${count}</td>
+            <td class="menu__item menu__item--login">${loginValue}</td>
+            <td class="menu__item menu__item--password">${passwordValue}</td>
+            <td class="menu__item menu__item--mail">${mailValue}</td>
+            <td class="menu__item menu__item--edit"><button id="edit" value="Edit">Edit</button></td>
+            <td class="menu__item menu__item--remove"><button id="remove" value="Remove">Remove</button></td>
+        `;
+    tableUsersWrapper.appendChild(tableRow);
+
+    f1.login.value = '';
+    f1.password.value = '';
+    f1.mail.value = '';
+})
+
+function deleteRow(row) {
+    document.getElementById('tableUsers').deleteRow(row);
+}
+
+function editRow(row) {
+    let arrayCols = row.querySelectorAll('td');
+
+    let loginCol = arrayCols[1].innerHTML;
+    let passwordCol = arrayCols[2].innerHTML;
+    let mailCol = arrayCols[3].innerHTML;
+
+    f1.login.value = loginCol;
+    f1.password.value = passwordCol;
+    f1.mail.value = mailCol;
+
+    btnAddUser.classList.remove('active');
+    btnEditUser.classList.add('active');
+
+    btnEditUser.addEventListener('click', () => {
+        btnAddUser.classList.add('active');
+        btnEditUser.classList.remove('active');
+
+        row.innerHTML = `
+            <td class="menu__item menu__item--number">${row.rowIndex}</td>
+            <td class="menu__item menu__item--login">${f1.login.value}</td>
+            <td class="menu__item menu__item--password">${f1.password.value}</td>
+            <td class="menu__item menu__item--mail">${f1.mail.value}</td>
+            <td class="menu__item menu__item--edit"><button id="edit" value="Edit">Edit</button></td>
+            <td class="menu__item menu__item--remove"><button id="remove" value="Remove">Remove</button></td>
+        `;
+    })
+}
+
+function tableClick(e) {
+    if(!e)
+        e = window.event;
+    if (e.target.value === "Remove") {
+        deleteRow( e.target.parentNode.parentNode.rowIndex );
+    }
+
+    if (e.target.value === "Edit") {
+        editRow(e.target.parentNode.parentNode);
+    }
+}
+
+*/
+
+
+
+/* ---------- 14 -------------
+const createEl = document.querySelector('.create');
+const coord = document.querySelector('.coord');
+
+createEl.addEventListener('click', function () {
+    const valueWidth = f1.width.value;
+    const valueHeight = f1.height.value;
+    const valueColor = f1.color.value;
+
+    let box = document.createElement("div");
+    box.innerHTML = "SUKA";
+    box.style.width = f1.width.value + 'px';
+    box.style.height = f1.height.value + 'px';
+    box.style.backgroundColor = valueColor;
+
+    let wrapper = document.querySelector('.wrapper');
+    wrapper.appendChild(box);
+
+    console.log(valueHeight);
+    console.log(valueWidth);
+
+})
+
+*/
+
+/* ----------------- 15 -----------------
+const createEl = document.querySelector('.add');
+let newWord = document.querySelector('.badwords')
+
+createEl.addEventListener('click', function () {
+    const word = f1.word.value;
+
+    newWord.innerHTML += word + ', ';
+
+})
+
+const censor = document.querySelector('.censor');
+
+censor.addEventListener('click', (e) => {
+
+    let i;
+    let mas = newWord.innerHTML.split(', ');
+
+    let text = f1.textarea.value;
+    let CHAR = '*';
+
+    const censored = text
+        .split(' ' || ', ')
+        .map(item => mas.includes(item) ? CHAR.repeat(item.length) : item)
+        .join(' ');
+
+    f1.textarea.value = censored;
+    console.log(censored);
+})*/
+
